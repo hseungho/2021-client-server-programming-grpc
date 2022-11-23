0819b7c13c60e5a6900f08a3030cff18a4f41dde
Seungho <hsh47607@naver.com>
2022-11-23 14:22:10 +0900

    ADD: make directories and classes, proto

---
+++ grpc-client/src/main/java/ClientGrpcMain.java
@@ -0,0 +1,5 @@
+public class ClientGrpcMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ grpc-server/src/main/java/ServerGrpcMain.java
@@ -0,0 +1,5 @@
+public class ServerGrpcMain {
+    public static void main(String[] args) {
+
+    }
+}
