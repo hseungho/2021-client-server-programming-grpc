03a1b9e39c57f0f5481acd223b585d2b93413105
Seungho <hsh47607@naver.com>
2022-12-21 18:41:29 +0900

    modify: open session to entitymanager for persist

--- grpc-server/src/main/java/config/HibernateConfig.java
+++ grpc-server/src/main/java/config/HibernateConfig.java
@@ -1,10 +1,19 @@
 package config;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 public class HibernateConfig {
+
+    private static HibernateConfig instance;
+    public static HibernateConfig getInstance() {
+        if(instance==null)
+            instance = new HibernateConfig();
+        return instance;
+    }
 
     private static SessionFactory sessionFactory;
     public HibernateConfig() {
@@ -13,9 +22,23 @@ public class HibernateConfig {
                 .buildSessionFactory();
     }
 
-    public static Session getSession() {
-        if(sessionFactory == null) new HibernateConfig();
-        return sessionFactory.openSession();
+    @PersistenceContext
+    private EntityManager em;
+    public static EntityManager entityManager() {
+        return sessionFactory.createEntityManager();
+    }
+
+    public <T> T findById(Class<T> type, Long id) {
+        em = HibernateConfig.entityManager();
+        em.getTransaction().begin();
+        T value = em.find(type, id);
+        em.persist(value);
+        em.getTransaction().commit();
+        return value;
+    }
+
+    public <T> void save(T object) {
+
     }
 
 }
--- grpc-server/src/main/java/repository/StudentRepository.java
+++ grpc-server/src/main/java/repository/StudentRepository.java
@@ -2,21 +2,13 @@ package repository;
 
 import config.HibernateConfig;
 import entity.Student;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 public class StudentRepository {
 
-    private Session session;
-    private Transaction tx;
+    private HibernateConfig hibernate = HibernateConfig.getInstance();
 
     public Student findById(Long id) {
-        session = HibernateConfig.getSession();
-        tx = session.beginTransaction();
-        Student student = session.get(Student.class, id);
-        tx.commit();
-        session.close();
-        return student;
+        return hibernate.findById(Student.class, id);
     }
 
 }
