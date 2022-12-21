060576f3cf597d365ff354b369d064152534647d
Seungho <hsh47607@naver.com>
2022-12-21 17:40:18 +0900

    add: assertj and modify test code to assertEquals

--- grpc-server/src/test/java/HibernateTest.java
+++ grpc-server/src/test/java/HibernateTest.java
@@ -1,7 +1,10 @@
 import entity.Student;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import repository.StudentRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HibernateTest {
 
@@ -12,8 +15,19 @@ public class HibernateTest {
         studentRepository = new StudentRepository();
     }
     @Test
+    @DisplayName("find student by id test")
     public void findStudentById() {
-        Student student = studentRepository.findById(1L);
-        System.out.println(student);
+        Student student_1 = new Student();
+        student_1.setStudentId("20100123");
+        student_1.setFirstName("Hwang");
+        student_1.setLastName("Myunghan");
+        student_1.setDepartment("CS");
+
+        Student student_2 = studentRepository.findById(1L);
+
+        assertEquals(student_2.getStudentId(), student_1.getStudentId());
+        assertEquals(student_2.getFirstName(), student_1.getFirstName());
+        assertEquals(student_2.getLastName(), student_1.getLastName());
+        assertEquals(student_2.getDepartment(), student_1.getDepartment());
     }
 }
